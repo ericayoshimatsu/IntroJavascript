41336c871e6ea35cddadc71fa61525c1195c4b47
Erica <ericayoshimatsu@gmail.com>
2020-08-15 10:14:59 +0200

    added variables, constants and operations

--- HelloWorld.js
+++ HelloWorld.js
@@ -5,10 +5,36 @@ Hello World file
 --------------------------------------
 */
 
+// constants
+const G = 9.81;
+// not allowed to change. for example G = 2; ist not allowed
 
-// varaible
-var i = 5;
+// variable
+var number = 5;
+var decimal = 5.10;
+var boolean = false;
+var string = "TEXT";
+
+// change variables 
+number = 7;
+
+// operators
+// Assignment: =
+// Arithmetic: + - * / % (+= -=)
+var remainder =  12 % number;
+number += 1;
+
+string += "_FULL"
+// Comparison: == != > >= < >=
+var compared = 5 == 5.0
+// Logical: || && !
 
 // print
-console.log(i);
 console.log("HI WORLD");
+console.log(number);
+console.log(decimal);
+console.log(boolean);
+console.log(string);
+console.log(G);
+console.log(remainder);
+console.log(compared);
