368c605bf2106d2162c842aab858c1a0e0df1e32
Erica Yoshimatsu <ericayoshimatsu@gmail.com>
2020-08-30 21:05:07 +0200

    Hello World expanded

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,12 +2,25 @@
 
 Hello World file 
 
+Install Node.js
+Install Add-On Code Runner
+
 --------------------------------------
 */
 
 // constants
 const G = 9.81;
 // not allowed to change. for example G = 2; ist not allowed
+
+// let is scope-based. Can only be accessed in the scope where it is defined
+if (true) {
+    var localScope = false;
+    let localScopeLet = false;
+    localScopeLet = true;
+    console.log(localScopeLet);
+}
+console.log(localScope);
+// console.log(localScopeLet); won't run
 
 // variable
 var number = 5;
@@ -29,12 +42,22 @@ string += "_FULL"
 var compared = 5 == 5.0
 // Logical: || && !
 
+// ternary operator
+//condition? true : false
+
+let ternary = true ? 1:0;
+console.log(`ternary = ${ternary}`);
+
+
 // print
 console.log("HI WORLD");
 console.log(number);
 console.log(decimal);
 console.log(boolean);
-console.log(string);
+console.log(string.toLowerCase());
 console.log(G);
 console.log(remainder);
 console.log(compared);
+// string interpolation `` (backticks + ${})
+let message = `number ${number}`;
+console.log(message);
