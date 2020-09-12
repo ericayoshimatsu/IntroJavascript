6cecc01891ff404fbd2b61e12feb1d93ad3f0572
Erica Yoshimatsu <ericayoshimatsu@gmail.com>
2020-09-12 13:51:33 +0200

    Arrays and Functions expanded

--- Arrays.js
+++ Arrays.js
@@ -18,3 +18,18 @@ console.log(shoppingList);
 var lastElement = shoppingList.pop();
 
 console.log(lastElement);
+
+console.log(shoppingList.length);
+
+// multidimensional array
+var levels = [
+    [1.1, 1.2, 1.3],
+    [2.1, 2.2, 2.3, 2.4],
+    [3.1, 3.2],
+];
+
+console.log(levels[0]);
+console.log(levels[1][3]);
+
+levels[1][3] = 5.5;
+console.log(levels[1]);
--- Functions.js
+++ Functions.js
@@ -14,3 +14,28 @@ function move (pos, byAmount)
 finalPos = move(0, 10);
 
 console.log(finalPos);
+
+var currentHealth = 50;
+var maxHealth = 200;
+
+console.log("Health = " + currentHealth);
+
+function heal(healAmount=10) {
+    var newHealth = currentHealth + healAmount;
+    currentHealth = newHealth > maxHealth? maxHealth : newHealth;
+    // function nested() {
+
+    //}
+    // nested functions can only be called within the main function
+    return (currentHealth / maxHealth) * 100;
+}
+
+var percentage = heal(50);
+console.log("Health = " + currentHealth + " = " + percentage + "%");
+percentage = heal();
+console.log("Health = " + currentHealth + " = " + percentage + "%");
+
+// function as parameter
+//var func = function heal2() {
+//    currentHealth += healAmount;
+//}
